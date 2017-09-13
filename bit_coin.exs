9ee85c6763beb8233b1a2bedbbcca1bc74994477
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-09-13 03:14:56 -0400

    added the changes to make the code work on multiple cores of the machine

--- bit_coin.exs
+++ bit_coin.exs
@@ -1,24 +1,40 @@
 defmodule BitCoin do
-    def mine(k \\ 3) do
+    def start_mining(map, k \\ 3) do
+        parent = self()
+        # Spawns the function which mines the bit coin
+        spawn fn -> mine(k, parent) end
+        # receiving message from child
+        map = receive do
+            {:good_coin, coin_details} -> check_coin_validity(elem(coin_details, 0), elem(coin_details, 1), map)
+        end
+        start_mining(map, k)
+    end
+    def mine(k, parent) do
         prefix = String.duplicate("0", k)
         random_string = generate_string()
         coin_hash = generate_hash(random_string)
-        #IO.puts "Received sha256: " <> coin_hash <> prefix
-        case valid_coin(coin_hash, prefix) do
-            true -> IO.puts "Bit Coin Found: " <> random_string <> " " <> coin_hash
-            false -> "Bit coin not found"
+        if valid_coin(coin_hash, prefix) do
+            # send message to parent process
+            send(parent, {:good_coin, {random_string, coin_hash}})
         end
-        mine(k)
+        mine(k, parent) #{}"Bit coin not found"
+    end
+    # Checks the validity of coin and adding new coin to map
+    defp check_coin_validity(coin_str, coin_hash, map) do
+        if Map.has_key?(map, coin_str) do
+            map
+        else
+            IO.puts "Bit coin: #{coin_str} #{coin_hash}"
+            Map.put(map, coin_str, coin_hash)
+        end
     end
     defp generate_string(length \\ 23) do
         random_str = :crypto.strong_rand_bytes(length) |> Base.encode64 |> binary_part(0, length)
-        #IO.puts "Random string: " <> random_str
         "22163183;" <> random_str
     end
 
     defp generate_hash(random_str) do
         str_hash = :crypto.hash(:sha256, random_str) |> Base.encode16 |> String.downcase
-        #IO.puts "Generated hash: " <> str_hash
         str_hash
     end
     # Returns true if the prefix is the start of the coin hash else false
@@ -26,4 +42,4 @@ defmodule BitCoin do
         String.starts_with? coin_hash, prefix
     end
 end
-BitCoin.mine()
+BitCoin.start_mining(%{}, 3)
