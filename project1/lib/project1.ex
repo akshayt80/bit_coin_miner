0b599beab632b026731a87e3b03bbdccd3e159c8
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-09-17 01:03:10 -0400

    added support to run server and client modules from project1 module based on command line argument

--- project1/lib/project1.ex
+++ project1/lib/project1.ex
@@ -14,14 +14,23 @@ defmodule Project1 do
   """
   def main(args) do
     {_, [str], _} = OptionParser.parse(args)
-    k = elem(Integer.parse(str), 0)
-    IO.puts is_integer(k)
-    Server.start_link(k)
+    if valid_ip(str) do
+      server_ip = parse_ip(str)
+      IO.puts server_ip |> Tuple.to_list |> Enum.join(".")
+      Client.start_link(server_ip)
+    else
+      # k is number of leading 0s
+      k = elem(Integer.parse(str), 0)
+      Server.start_link(k)
+    end
   end
 
-  defp parse_args(args) do
-    # Covering only expected arguments. We can add code to handle cases when unexpected arguments are passed or help text etc.
-    # Checkout OptionParser lib for details. 
-    
+  defp valid_ip(str) do
+    str =~ "."
+  end
+  defp parse_ip(str) do
+    # convert input string 127.0.0.1 to tuple of integers like {127, 0, 0, 1}
+    [a, b, c, d] = String.split(str, ".")
+    {String.to_integer(a), String.to_integer(b), String.to_integer(c), String.to_integer(d)}
   end
 end
