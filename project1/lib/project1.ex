037a96512185beafdd6859e773f01dd919952d9b
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-09-17 09:02:57 -0400

    define port number in begining and using number of cores to spawn number of processes

--- project1/lib/project1.ex
+++ project1/lib/project1.ex
@@ -14,13 +14,18 @@ defmodule Project1 do
   """
   def main(args) do
     {_, [str], _} = OptionParser.parse(args)
+    # Using number of cores to spawn mining processes and multiplying 3 to make
+    # sure if any process dies due to any reason we have other process to take its place
+    cores = System.schedulers_online * 3
+    # Port on which tcp communication will hapen
+    port = 6666
     if valid_ip(str) do
       server_ip = parse_ip(str)
-      Client.start_link(server_ip)
+      Client.start_link(server_ip, cores, port)
     else
       # k is number of leading 0s
       k = elem(Integer.parse(str), 0)
-      Server.start_link(k)
+      Server.start_link(k, cores, port)
     end
   end
 
