be24567c3fecae6072be1f5e018b61a236044f9a
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-09-17 01:43:55 -0400

    Printing start of client process and removed ither print statements

--- project1/lib/client.ex
+++ project1/lib/client.ex
@@ -1,10 +1,10 @@
 defmodule Client do
     def start_link(server_ip, port \\ 6666) do
+        IO.puts "Starting client process"
         # Connect to server
         {:ok, socket} = :gen_tcp.connect(server_ip, port, [:binary, {:active, false},{:packet, 0}])
         # Receive k value from server
         {:ok, data} = :gen_tcp.recv(socket, 0)
-        IO.puts "k value: #{data}"
         parent = self()
         k = elem(Integer.parse(data), 0)
         start_mining(socket, k, parent)
