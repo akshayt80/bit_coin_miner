932f2a05367f95a0e32952ed120be17c07274c5a
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-09-17 09:04:38 -0400

    using computed cores to spwan mining processes

--- project1/lib/client.ex
+++ project1/lib/client.ex
@@ -1,6 +1,6 @@
 defmodule Client do
 
-    def start_link(server_ip, port \\ 6666) do
+    def start_link(server_ip, cores, port) do
         IO.puts "Starting client process"
         # Connect to server
         {:ok, socket} = :gen_tcp.connect(server_ip, port, [:binary, {:active, false},{:packet, 0}])
@@ -8,20 +8,24 @@ defmodule Client do
         {:ok, data} = :gen_tcp.recv(socket, 0)
         parent = self()
         k = elem(Integer.parse(data), 0)
-        start_mining(socket, k, parent)
+        start_mining(socket, k, parent, cores)
     end
 
     defp send_coin(server, data) do
         ok = server |> :gen_tcp.send(data)
     end
 
-    defp start_mining(server, k, parent) do
-        # Spawn the mining process separate
-        spawn fn -> BitCoin.mine(k, parent) end
+    defp start_mining(server, k, parent, cores) do
+        # Spawn the mining processes separately
+        for n <- 1..cores, do: spawn fn -> BitCoin.mine(k, parent) end
+        listener(server)
+    end
+
+    defp listener(server) do
+        # send coins over tcp after we receive any valid coin from some process
         receive do
             {:good_coin, coin_details} -> send_coin(server, coin_details)
         end
-        start_mining(server, k, parent)
+        listener(server)
     end
-
 end
